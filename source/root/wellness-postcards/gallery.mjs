8bbd38408419c491aa7cb4d1f1990379fdb6b707
JavaGT <user@Users-Laptop.local>
2024-11-07 10:23:21 +1300

    postcards

--- source/root/wellness-postcards/gallery.mjs
+++ source/root/wellness-postcards/gallery.mjs
@@ -19,35 +19,66 @@ canvas.style.display = 'none';
 // for (let i=0;i<10;i++) {
 // create a card for each row in the csv
 data
-// randomize the order of the rows
-.sort((a, b) => Math.random() - 0.5)
-.forEach(row => {
-    row = JSON.parse(row.JSON);
-    if (!row.message?.trim()) return;
-    const formdata = new FormData();
-    formdata.append('prompt', row.prompt);
-    formdata.append('image', row.image);
-    formdata.append('message', row.message);
-    formdata.append('signed', row.signed);
-    formdata.append('stamp', row.stamp || 'WATU');
+    // randomize the order of the rows
+    .sort((a, b) => Math.random() - 0.5)
+    .forEach(row => {
+        if (row.Remove.toLowerCase() === 'yes') {
+            console.log('Skipping row', row);
+            return
+        }
+        if (!row.JSON) return;
+        try {
+            row = JSON.parse(row.JSON);
+        } catch (e) {
+            console.error('Error parsing JSON', row.JSON);
+            return;
+        }
+        if (!row.message?.trim()) return;
 
-    // render the postcard
-    renderPostcard(formdata, ctx, { stamps, images });
-    // append the postcard to the gallery
-    const img = new Image();
-    img.src = canvas.toDataURL();
-    gallery.appendChild(img);
+        if (shouldHide(row.message)) return
 
-    // when image is clicked, open its dataurl in a new tab
-    img.style.cursor = 'pointer';
-    img.addEventListener('click', () => {
-        const win = window.open();
-        win.document.write(`<img src="${img.src}">`);
+        console.log('Rendering row', row);
+        const formdata = new FormData();
+        formdata.append('prompt', row.prompt);
+        formdata.append('image', row.image);
+        formdata.append('message', row.message);
+        formdata.append('signed', row.signed);
+        formdata.append('stamp', row.stamp || 'WATU');
+
+        // render the postcard
+        renderPostcard(formdata, ctx, { stamps, images });
+        // append the postcard to the gallery
+        const img = new Image();
+        img.src = canvas.toDataURL();
+        gallery.appendChild(img);
+
+        // when image is clicked, open its dataurl in a new tab
+        img.style.cursor = 'pointer';
+        img.addEventListener('click', () => {
+            const win = window.open();
+            win.document.write(`<img src="${img.src}">`);
+        });
+        // add id to the image
+        img.id = row.submission_id
     });
-    // add id to the image
-    img.id = row.submission_id
-});
 // }
 
 // remove the loading spinner id="gallery-loading"
 document.querySelector('#gallery-loading').remove();
+
+
+const hidewords = [
+    'communist',
+    'socialist',
+    'lazy',
+    'privliged',
+]
+
+function shouldHide(message) {
+    for (const bad of hidewords) {
+        if (message.toLowerCase().includes(bad)) {
+            return true
+        }
+    }
+    return false
+}
